b226d67882aa50bdac74bfcbdceb0cbda02f1b4f
Jokubas123456 <jokubas.zaveckis@gmail.com>
2022-11-13 00:58:41 +0200

    Enemy

---
+++ Knygnesys/Assets/Scripts/Enemies/HeadDetectScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadDetectScript : MonoBehaviour
+{
+    GameObject Enemy;
+    public static int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+    void Start()
+    {
+        Enemy = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GetComponent<Collider2D>().enabled = false;
+        Enemy.GetComponent<SpriteRenderer>().flipY = true;
+        Enemy.GetComponent<BoxCollider2D>().enabled = false;
+        /*Vector3 movement = new Vector3(Random.Range(40, 70), Random.Range(-40, 40), 0f);
+        Enemy.transform.position = Enemy.transform.position + movement * Time.deltaTime;
+        //transform.Translate(userDirection * movespeed * Time.deltaTime);*/
+        //transform.Translate(Vector2.down * Time.deltaTime);
+
+        Enemy.transform.position = new Vector3(0f, 0f, 0f);
+        //Destroy(this.gameObject);
+        
+
+    }
+}
