ecbdb90827745dbfca7e9326ffade573fb3a0959
simonassimenas <simonas.simenas@gmail.com>
2022-11-16 12:56:47 +0200

    Bandziau...

--- Knygnesys/Assets/Scripts/Enemies/Weapon.cs
+++ Knygnesys/Assets/Scripts/Enemies/Weapon.cs
@@ -7,14 +7,12 @@ public class Weapon : MonoBehaviour
     public float range;
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public LayerMask layerMask = 1;
 
     void Update()
     {
-        RaycastHit2D playerInfo = Physics2D.Raycast(firePoint.position, firePoint.right, range);
-        if(playerInfo == true)//(playerInfo.collider.gameObject.CompareTag("Player") == true)
-        //problema kaip detectint kad pamato playeri
-        //reiktu dar padaryt cooldown tarp suviu
-        //cia pas mane kiek sudinokai
+        RaycastHit2D playerInfo = Physics2D.Raycast(firePoint.position, firePoint.right, range, layerMask);
+        if(playerInfo.collider != null)
         {
             Shoot();
         }
