c613cad7d726f5ba85a85d8f81d226b76ff00681
Pranas Zeromskas <76076765+Zeromskas@users.noreply.github.com>
2022-11-30 01:10:37 +0200

    Minor enemy fixes and animations

--- Knygnesys/Assets/Scripts/Enemies/HeadDetectScript.cs
+++ Knygnesys/Assets/Scripts/Enemies/HeadDetectScript.cs
@@ -16,14 +16,14 @@ public class HeadDetectScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GetComponent<Collider2D>().enabled = false;
-        Enemy.GetComponent<SpriteRenderer>().flipY = true;
+        Enemy.GetComponent<SpriteRenderer>().enabled = false;
         Enemy.GetComponent<BoxCollider2D>().enabled = false;
         /*Vector3 movement = new Vector3(Random.Range(40, 70), Random.Range(-40, 40), 0f);
         Enemy.transform.position = Enemy.transform.position + movement * Time.deltaTime;
         //transform.Translate(userDirection * movespeed * Time.deltaTime);*/
         //transform.Translate(Vector2.down * Time.deltaTime);
 
-        Enemy.transform.position = new Vector3(0f, 0f, 0f);
+        Enemy.transform.position = new Vector3(0f, -70f, 0f);
         //Destroy(this.gameObject);
         
 
--- Knygnesys/Assets/Scripts/Enemies/Weapon.cs
+++ Knygnesys/Assets/Scripts/Enemies/Weapon.cs
@@ -2,35 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Weapon : MonoBehaviour
 {
     public float range;
     public Transform firePoint;
     public GameObject bulletPrefab;
     public LayerMask layerMask = 1;
+    public Animator animator;
+    float period=0.0f;
+    public int shootingDelay = 3;
+    
 
     private float timer;
 
     void Update()
     {
         //RaycastHit2D playerInfo = Physics2D.Raycast(firePoint.position, firePoint.right, range, layerMask);
-        timer += Time.deltaTime;
+
+        // timer += Time.deltaTime;
+
         /*if(playerInfo.collider != null)
         {
             Shoot();
         }*/
 
-        if(timer>1)
-        {
-            timer = 0;
-            Shoot();
-        }
-    }
+        // if(timer>1)
+        // {
+        //     timer = 0;
+        //     Shoot();
+        // }
 
+        if (period>shootingDelay)
+        {
+            Shoot();
+            period = 0;
+        }
+        period += UnityEngine.Time.deltaTime;
+    }
+    
     void Shoot()
     {
+        animator.SetBool("isShooting", true);
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        animator.SetBool("isShooting", false);
     }
 
+   
 
 }
