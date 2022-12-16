6e370dc211358eab2c0ae534d2e70dece71d82b3
Pranas Zeromskas <76076765+Zeromskas@users.noreply.github.com>
2022-12-16 13:29:49 +0200

    nebepamenu bet kazkas ko reikejo

--- Knygnesys/Assets/Scripts/AudioOutput.cs
+++ Knygnesys/Assets/Scripts/AudioOutput.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class AudioOutput : MonoBehaviour
 {
+    public Animator animacija;
     public void MuteToggle (bool muted)
     {
         if (muted)
         {
             AudioListener.volume = 0;
+            animacija.SetBool("AudioIsON", false);
         }
         else
         {
             AudioListener.volume = 1;
+            animacija.SetBool("AudioIsON", true);
         }
     }
 }
