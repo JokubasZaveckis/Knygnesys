50e324e15074d3d3a554ddc33994a027edc57283
simonassimenas <simonas.simenas@gmail.com>
2022-12-18 22:59:04 +0200

    Game Scaling

--- Knygnesys/Assets/Scripts/CameraScript.cs
+++ Knygnesys/Assets/Scripts/CameraScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
     public Transform target;
 
@@ -14,9 +14,25 @@ public class Camera : MonoBehaviour
 
     private float size;
 
+    public SpriteRenderer outline;
+
     void Start()
     {
         size = siena.GetComponent<BoxCollider2D>().size.y; //kintamojo dydis backgroundo box colliderio dydzio
+
+        float screenRatio = (float)Screen.width / (float)Screen.height;
+        float targetRatio = outline.bounds.size.x / outline.bounds.size.y;
+
+        if(screenRatio >= targetRatio)
+        {
+            Camera.main.orthographicSize = outline.bounds.size.y / 2;
+        }
+        
+        else
+        {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = outline.bounds.size.y / 2 * differenceInSize;
+        }
     }
 
     void Update()
